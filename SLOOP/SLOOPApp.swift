3b65b8460608e10ce10b6193736bd56288faf982
Mirinwino <52835948+Mirinwino@users.noreply.github.com>
2022-04-20 12:22:04 +0200

    Initial Commit

---
+++ SLOOP/SLOOPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SLOOPApp.swift
+//  SLOOP
+//
+//  Created by Miriam Peinado on 20/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SLOOPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
