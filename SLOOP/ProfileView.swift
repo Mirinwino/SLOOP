d97caac8e64a6396314ece95324b3949b33f185e
Anastasia Sarlidou <anactaciaxd@gmail.com>
2022-04-27 12:54:37 +0200

    Added navigation  from profile to data

--- SLOOP/ProfileView.swift
+++ SLOOP/ProfileView.swift
@@ -12,47 +12,49 @@ struct ProfileView: View {
     @State private var navigationIsShowing = false
     
     var body: some View {
-        VStack{
-            ZStack(alignment: .center){
-                Rectangle()
-                    .fill(.linearGradient(
-                        Gradient(colors: [Color("AppLightTeal"), Color("AppDarkTeal")]),
-                        startPoint: UnitPoint(x: 0.5, y: 0),
-                        endPoint: UnitPoint(x: 0.5, y: 0.6)))
-                    .frame(width: .infinity, height: 200)
-                    .edgesIgnoringSafeArea(.top)
-                Text("Ema Watson")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.white)
-            }
-            Spacer()
-            VStack(spacing: 12){
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(Color("AppDarkTeal"))
-                    Text("Birthday")
-                        .fontWeight(.light)
-                        .foregroundColor(Color("AppDarkTeal"))
-                    Spacer()
-                    Text("01/01/1990")
-                        .fontWeight(.light)
-                        .foregroundColor(Color("AppDarkTeal"))
+        NavigationView{
+            
+            VStack{
+                ZStack(alignment: .center){
+                    Rectangle()
+                        .fill(.linearGradient(
+                            Gradient(colors: [Color("AppLightTeal"), Color("AppDarkTeal")]),
+                            startPoint: UnitPoint(x: 0.5, y: 0),
+                            endPoint: UnitPoint(x: 0.5, y: 0.6)))
+                        .frame(width: .infinity, height: 200)
+                        .edgesIgnoringSafeArea(.top)
+                    Text("Ema Watson")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
                 }
-                .padding()
-                .frame(width: 320, height: 60)
-                .background(RoundedRectangle(cornerRadius: 8)
-                    .fill(Color("AppLightTeal"))
-                    .shadow(radius: 2))
-                
-                HStack{
-                    Image(systemName: "heart.text.square")
-                        .foregroundColor(Color("AppDarkTeal"))
-                    Spacer()
-                    Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Apple health Sync")/*@END_MENU_TOKEN@*/.fontWeight(.light)
+                Spacer()
+                VStack(spacing: 12){
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(Color("AppDarkTeal"))
+                        Text("Birthday")
+                            .fontWeight(.light)
+                            .foregroundColor(Color("AppDarkTeal"))
+                        Spacer()
+                        Text("01/01/1990")
+                            .fontWeight(.light)
                             .foregroundColor(Color("AppDarkTeal"))
                     }
+                    .padding()
+                    .frame(width: 320, height: 60)
+                    .background(RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("AppLightTeal"))
+                        .shadow(radius: 2))
+                    
+                    HStack{
+                        Image(systemName: "heart.text.square")
+                            .foregroundColor(Color("AppDarkTeal"))
+                        Spacer()
+                        Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Apple health Sync")/*@END_MENU_TOKEN@*/.fontWeight(.light)
+                                .foregroundColor(Color("AppDarkTeal"))
+                        }
                 }
                 .padding()
                 .frame(width: 320, height: 60)
@@ -77,22 +79,28 @@ struct ProfileView: View {
 
 
                 Button(action: {}) {
+                    
                     HStack{
-                        Image(systemName: "doc.text")
-                            .foregroundColor(Color("AppDarkTeal"))
-                        Text("About Sloop and my data")
-                            .fontWeight(.light)
-                            .foregroundColor(Color("AppDarkTeal"))
                         Spacer()
-
-                    }
-                    .padding()
-                    .frame(width: 320, height: 60)
-                    .background(RoundedRectangle(cornerRadius: 8)
-                        .fill(Color("AppLightTeal"))
-                        .shadow(radius: 2))
                         
-                    }
+                        NavigationLink(destination: EthicsView()) {
+                            
+                            Image(systemName: "doc.text")
+                                .foregroundColor(Color("AppDarkTeal"))
+                            Text("About Sloop and my data")
+                                .fontWeight(.light)
+                                .foregroundColor(Color("AppDarkTeal"))
+                        }.padding()
+                         .frame(width: 320, height: 60, alignment: .topLeading)
+                      
+                                
+                            }
+
+                    }.padding()
+                     .frame(width: 320, height: 60)
+                     .background(RoundedRectangle(cornerRadius: 8)
+                     .fill(Color("AppLightTeal"))
+                     .shadow(radius: 2))
                 
                 HStack{
                     Image(systemName: "arrow.down.doc")
@@ -114,6 +122,8 @@ struct ProfileView: View {
             Spacer()
         }
     }
+    
+}
 }
 
 struct ProfileView_Previews: PreviewProvider {
