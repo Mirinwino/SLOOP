13596f8fe01d25a0ed16018c9438572961e4c195
Mirinwino <52835948+Mirinwino@users.noreply.github.com>
2022-04-27 11:50:45 +0200

    Solved profile and ethics view for different size devices

--- SLOOP/ProfileView.swift
+++ SLOOP/ProfileView.swift
@@ -12,75 +12,92 @@ struct ProfileView: View {
     @State private var navigationIsShowing = false
     
     var body: some View {
-        ZStack{
-            Rectangle()
-                .fill(.linearGradient(
-                    Gradient(colors: [Color("AppLightTeal"), Color("AppDarkTeal")]),
-                    startPoint: UnitPoint(x: 0.5, y: 0),
-                    endPoint: UnitPoint(x: 0.5, y: 0.6)))
-                .frame(width: .infinity, height: 300)
-                .position(CGPoint(x:195, y: 10))
-            
-            Text("Ema Watson")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.white)
-                .position(CGPoint(x:195, y: 100))
-            
-        VStack(spacing: 12){
-            HStack{
-                Image(systemName: "person")
-                    .foregroundColor(Color("AppDarkTeal"))
-                Text("Birthday")
-                    .fontWeight(.light)
-                    .foregroundColor(Color("AppDarkTeal"))
-                Spacer()
-                Text("01/01/1990")
-                    .fontWeight(.light)
-                    .foregroundColor(Color("AppDarkTeal"))
+        VStack{
+            ZStack(alignment: .center){
+                Rectangle()
+                    .fill(.linearGradient(
+                        Gradient(colors: [Color("AppLightTeal"), Color("AppDarkTeal")]),
+                        startPoint: UnitPoint(x: 0.5, y: 0),
+                        endPoint: UnitPoint(x: 0.5, y: 0.6)))
+                    .frame(width: .infinity, height: 200)
+                    .edgesIgnoringSafeArea(.top)
+                Text("Ema Watson")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
             }
-            .padding()
-            .frame(width: 320, height: 60)
-            .background(RoundedRectangle(cornerRadius: 8)
-                .fill(Color("AppLightTeal"))
-                .shadow(radius: 2))
-            
-            HStack{
-                Image(systemName: "heart.text.square")
-                    .foregroundColor(Color("AppDarkTeal"))
-                Spacer()
-                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Apple health Sync")/*@END_MENU_TOKEN@*/.fontWeight(.light)
-                        .foregroundColor(Color("AppDarkTeal"))
-                }
-            }
-            .padding()
-            .frame(width: 320, height: 60)
-            .background(RoundedRectangle(cornerRadius: 8)
-                .fill(Color("AppLightTeal"))
-                .shadow(radius: 2))
-            
-            HStack{
-                Image(systemName: "bell.badge")
-                    .foregroundColor(Color("AppDarkTeal"))
-                Spacer()
-                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Notifications")/*@END_MENU_TOKEN@*/.fontWeight(.light)
-                        .foregroundColor(Color("AppDarkTeal"))
-                }
-            }
-            .padding()
-            .frame(width: 320, height: 60)
-            .background(RoundedRectangle(cornerRadius: 8)
-                .fill(Color("AppLightTeal"))
-                .shadow(radius: 2))
-
-
-            Button(action: {}) {
+            Spacer()
+            VStack(spacing: 12){
                 HStack{
-                    Image(systemName: "doc.text")
+                    Image(systemName: "person")
                         .foregroundColor(Color("AppDarkTeal"))
-                    Text("About Sloop and my data")
+                    Text("Birthday")
+                        .fontWeight(.light)
+                        .foregroundColor(Color("AppDarkTeal"))
+                    Spacer()
+                    Text("01/01/1990")
+                        .fontWeight(.light)
+                        .foregroundColor(Color("AppDarkTeal"))
+                }
+                .padding()
+                .frame(width: 320, height: 60)
+                .background(RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("AppLightTeal"))
+                    .shadow(radius: 2))
+                
+                HStack{
+                    Image(systemName: "heart.text.square")
+                        .foregroundColor(Color("AppDarkTeal"))
+                    Spacer()
+                    Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Apple health Sync")/*@END_MENU_TOKEN@*/.fontWeight(.light)
+                            .foregroundColor(Color("AppDarkTeal"))
+                    }
+                }
+                .padding()
+                .frame(width: 320, height: 60)
+                .background(RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("AppLightTeal"))
+                    .shadow(radius: 2))
+                
+                HStack{
+                    Image(systemName: "bell.badge")
+                        .foregroundColor(Color("AppDarkTeal"))
+                    Spacer()
+                    Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Notifications")/*@END_MENU_TOKEN@*/.fontWeight(.light)
+                            .foregroundColor(Color("AppDarkTeal"))
+                    }
+                }
+                .padding()
+                .frame(width: 320, height: 60)
+                .background(RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("AppLightTeal"))
+                    .shadow(radius: 2))
+
+
+                Button(action: {}) {
+                    HStack{
+                        Image(systemName: "doc.text")
+                            .foregroundColor(Color("AppDarkTeal"))
+                        Text("About Sloop and my data")
+                            .fontWeight(.light)
+                            .foregroundColor(Color("AppDarkTeal"))
+                        Spacer()
+
+                    }
+                    .padding()
+                    .frame(width: 320, height: 60)
+                    .background(RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("AppLightTeal"))
+                        .shadow(radius: 2))
+                        
+                    }
+                
+                HStack{
+                    Image(systemName: "arrow.down.doc")
+                        .foregroundColor(Color("AppDarkTeal"))
+                    Text("Download my data")
                         .fontWeight(.light)
                         .foregroundColor(Color("AppDarkTeal"))
                     Spacer()
@@ -91,26 +108,12 @@ struct ProfileView: View {
                 .background(RoundedRectangle(cornerRadius: 8)
                     .fill(Color("AppLightTeal"))
                     .shadow(radius: 2))
-                    
-                }
-            
-            HStack{
-                Image(systemName: "arrow.down.doc")
-                    .foregroundColor(Color("AppDarkTeal"))
-                Text("Download my data")
-                    .fontWeight(.light)
-                    .foregroundColor(Color("AppDarkTeal"))
-                Spacer()
-
             }
-            .padding()
-            .frame(width: 320, height: 60)
-            .background(RoundedRectangle(cornerRadius: 8)
-                .fill(Color("AppLightTeal"))
-                .shadow(radius: 2))
+            Spacer()
+            Spacer()
+            Spacer()
         }
     }
-}
 }
 
 struct ProfileView_Previews: PreviewProvider {
--- SLOOP/Views/EthicsView.swift
+++ SLOOP/Views/EthicsView.swift
@@ -9,44 +9,48 @@ import SwiftUI
 
 struct EthicsView: View {
     var body: some View {
-        ZStack{
-            Rectangle()
-                .fill(.linearGradient(
-                    Gradient(colors: [Color("AppLightTeal"), Color("AppDarkTeal")]),
-                    startPoint: UnitPoint(x: 0.5, y: 0),
-                    endPoint: UnitPoint(x: 0.5, y: 0.6)))
-                .frame(width: .infinity, height: 300)
-                .position(CGPoint(x:195, y: 10))
-            
-            
-            Button(action: {}) {
-                HStack {
-                    Image(systemName: "chevron.backward")
-                        .foregroundColor(Color("AppDarkTeal"))
+        VStack{
+            ZStack(alignment: .center){
+                Rectangle()
+                    .fill(.linearGradient(
+                        Gradient(colors: [Color("AppLightTeal"), Color("AppDarkTeal")]),
+                        startPoint: UnitPoint(x: 0.5, y: 0),
+                        endPoint: UnitPoint(x: 0.5, y: 0.6)))
+                    .frame(width: .infinity, height: 200)
+                    .edgesIgnoringSafeArea(.top)
+                HStack{
+                    Button(action: {}) {
+                        HStack {
+                            Image(systemName: "chevron.backward")
+                                .foregroundColor(Color("AppDarkTeal"))
+                        }
+                        .padding()
+                        .background(Color("AppLightTeal"))
+                        .clipShape(Capsule())
+                        
+                    }
+                    .frame(width: 40, height: 40)
+                    .cornerRadius(12)
+                    Spacer()
+                    Text("Data Privacy")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                    Spacer()
                 }
                 .padding()
-                .background(Color("AppLightTeal"))
-                .clipShape(Capsule())
-                
             }
-            .frame(width: 40, height: 40)
-            .cornerRadius(12)
-            .position(CGPoint(x:40, y: 100))
-            
-            
-            Text("Data Privacy")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.white)
-                .position(CGPoint(x:200, y: 100))
-
-            
+            Spacer()
             Text("Your sleep is unique and we want to personalize the app to help you reach your goals. To do this, we will store the data that you input and use it to suggest changes in behavior that may improve your sleep quality, based on when your tracked activity. Your data will not be shared with anyone else. We follow GDPR guidelines.")
                 .font(.body)
                 .fontWeight(.regular)
                 .multilineTextAlignment(.center)
                 .lineSpacing(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
                 .padding()
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
         }
     }
 }
